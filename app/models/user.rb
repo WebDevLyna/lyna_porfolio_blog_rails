06816fa1115ee0108e835e3891b2f2c01b2505ea
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-05-09 15:57:01 -0400

    Remove devise sign_up functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    # 1) removed "registerable" to disable visitors/public to sign up
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
