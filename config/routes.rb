dc7a8c5bc55c35ad9db743a093e25d9e232ac92c
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-05-09 16:11:35 -0400

    Style forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'wecome/index'
+
+  resources :posts
+  resources :projects
+  resources :contacts, only: [:new, :create]
+
   # 2) Automatically redirect to homepage if typing in random paths
     # if want to make new user logins, comment get '*path' line out
     # and # add "registerable" to enable visitors/public to users/sign_up
   get '*path' => redirect('/')
-  resources :posts
-  resources :projects
-  resources :contacts, only: [:new, :create]
 end
